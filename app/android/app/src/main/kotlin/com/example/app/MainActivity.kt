9946baa4d8d90067e1bdae064a4ae909daa046d6
KalilDev <kalilgamer0@gmail.com>
2022-12-04 16:38:37 -0300

    android: initial configuration

--- app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.app
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity: FlutterActivity() {
+    override  fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen();
+        super.onCreate(savedInstanceState);
+    }
 }
